4be036a306f2dfc5c019ff13fa80473e490c550d
Steven Chang <stevenchang5000@gmail.com>
2021-04-05 11:57:26 +1000

    Home page to be a list of services (#18)

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,12 @@
 
 Rails.application.routes.draw do
   get 'aliases/search', to: 'aliases#search'
-  resources :aliases, only: %i[index new]
+  resources :aliases, only: %i[new]
+  get 'farm-manager', to: 'farm_manager#home'
   resources :invoices, only: %i[index new show edit]
 
+  # === DATAHUB REVERSE PROXY ===
   match 'datahub/*path' => 'datahub#index', via: %i[get post put patch delete], defaults: { format: 'json' }
 
-  root 'aliases#search'
+  root 'application#home'
 end
