ff4a0b431040689f3fd58c77eb4df3a995a5a25e
Steven Chang <stevenchang5000@protonmail.com>
2021-05-14 13:47:05 +1000

    application.html: ico crypto paths to start from root (#77)
    * application.html: ico crypto paths to start from root
    
    * Remove Bootstrap 5
    
    Co-authored-by: Steven Chang <stevenchang5000@gmail.com>

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,19 +7,7 @@ import Rails from "@rails/ujs"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
-// BOOTSTRAP
-import 'bootstrap/js/src/alert'
-import 'bootstrap/js/src/button'
-import 'bootstrap/js/src/carousel'
-import 'bootstrap/js/src/collapse'
-import 'bootstrap/js/src/dropdown'
-import 'bootstrap/js/src/modal'
-import 'bootstrap/js/src/popover'
-import 'bootstrap/js/src/scrollspy'
-import 'bootstrap/js/src/tab'
-import 'bootstrap/js/src/toast'
-import 'bootstrap/js/src/tooltip'
-require("jquery")
+// BOOTSTRAP IS DEFINED AT BOTTOM OF APPLICATION.HTML.HAML
 
 // APP
 import '../src/block_locker/new'
