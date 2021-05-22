3ef11cf7ae04e79fbbe4aed04845280a9a677b28
Steven Chang <stevenchang5000@protonmail.com>
2021-05-22 11:07:45 +1000

    [bg-61] transactions: do not request to connect to wallet on page load (#153)
    Co-authored-by: Steven Chang <stevenchang5000@gmail.com>

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -29,6 +29,38 @@ import '../src/secret_network/transactions/index'
 Rails.start()
 ActiveStorage.start()
 
+// This needs to be called within an es6 function e.g. onload = () => etc
+document.connectWallet = function(chainId) {
+  // Keplr extension injects the offline signer that is compatible with cosmJS.
+  // You can get this offline signer from `window.getOfflineSigner(chainId:string)` after load event.
+  // And it also injects the helper function to `window.keplr`.
+  // If `window.getOfflineSigner` or `window.keplr` is null, Keplr extension may be not installed on browser.
+  if (!window.getOfflineSigner || !window.keplr) {
+    alert("Please install keplr extension");
+  } else {
+    if (window.keplr.experimentalSuggestChain) {
+      (async () => {
+        try {
+          // This method will ask the user whether or not to allow access if they haven't visited this website.
+          // Also, it will request user to unlock the wallet if the wallet is locked.
+          // If you don't request enabling before usage, there is no guarantee that other methods will work.
+          await window.keplr.enable(chainId);
+
+          // @ts-ignore
+          const keplrOfflineSigner = window.getOfflineSigner(chainId);
+          const accounts = await keplrOfflineSigner.getAccounts();
+          this.address = accounts[0].address;
+          $('#address').val(this.address);
+        } catch (error) {
+          console.error(error)
+        }
+      })();
+    } else {
+      alert("Please use the recent version of keplr extension");
+    }
+  }
+}
+
 document.featureContractAddress = function(environment) {
   let contractAddress
   if (environment == 'staging') {
--- app/javascript/src/secret_network/transactions/index.js
+++ app/javascript/src/secret_network/transactions/index.js
@@ -1,124 +1,92 @@
 $(document).ready(function(){
   if($("#secret-network-transactions").length) {
-    window.onload = async () => {
-      // initDatatable
-      this.datatable = window.$('#transactions-table').DataTable({
-        columns: [
-            { title: "ID" },
-            { title: "Description" },
-            { title: "Amount" },
-        ],
-        dom: '<"top"i>frtp',
-        ordering: false,
-        paging: false
-      });
-      initListeners()
-      this.chainId = 'secret-2';
-
-      // Keplr extension injects the offline signer that is compatible with cosmJS.
-      // You can get this offline signer from `window.getOfflineSigner(chainId:string)` after load event.
-      // And it also injects the helper function to `window.keplr`.
-      // If `window.getOfflineSigner` or `window.keplr` is null, Keplr extension may be not installed on browser.
-      if (!window.getOfflineSigner || !window.keplr) {
-        alert("Please install keplr extension");
-      } else {
-        if (window.keplr.experimentalSuggestChain) {
-          try {
-            // This method will ask the user whether or not to allow access if they haven't visited this website.
-            // Also, it will request user to unlock the wallet if the wallet is locked.
-            // If you don't request enabling before usage, there is no guarantee that other methods will work.
-            await window.keplr.enable(this.chainId);
-
-            // @ts-ignore
-            const keplrOfflineSigner = window.getOfflineSigner(this.chainId);
-            const accounts = await keplrOfflineSigner.getAccounts();
-            this.address = accounts[0].address;
-            $('#address').val(this.address);
-          } catch (error) {
-            console.error(error)
+    this.datatable = window.$('#transactions-table').DataTable({
+      columns: [
+          { title: "ID" },
+          { title: "Description" },
+          { title: "Amount" },
+      ],
+      dom: '<"top"i>frtp',
+      ordering: false,
+      paging: false
+    });
+    initListeners()
+    document.secretNetworkTransactionsForm.onsubmit = () => {
+      let transactions = [];
+      this.datatable.clear().draw();
+      // Disable form
+      $("#search-button").prop("disabled", true);
+      $("#loading").removeClass("d-none")
+      $("#ready").addClass("d-none")
+      document.hideAllAlerts();
+      (async () => {
+        try {
+          // Set environment
+          let environment = document.featureEnvironment();
+          let client =  document.secretNetworkClient(environment);
+          let address = document.secretNetworkTransactionsForm.address.value;
+          let contractAddress = secretNetworkTransactionsForm.contractAddress.value;
+          let chainId = document.secretNetworkChainId(environment);
+          let viewingKey = document.secretNetworkTransactionsForm.viewingKey.value;
+          let pageSize = document.secretNetworkTransactionsForm.pageSize.value;
+          let page = document.secretNetworkTransactionsForm.page.value;
+          let params = {
+            transfer_history: {
+              address: address,
+              key: viewingKey,
+              page: parseFloat(page - 1),
+              page_size: parseFloat(pageSize)
+            }
           }
-        } else {
-          alert("Please use the recent version of keplr extension");
+          let transactions_response = await client.queryContractSmart(contractAddress, params);
+          _.each(transactions_response["transfer_history"]["txs"], function(value){
+            let row = [];
+            // data
+            let id = value['id']
+            row.push(id);
+            description & amount
+            let amount = value['coins']['amount']
+            let description
+            if (address != value['receiver']) {
+              amount *= -1
+              description = value['receiver']
+            } else {
+              description = value['from']
+            }
+            row.push(description)
+            row.push(parseFloat(amount).toLocaleString('en'))
+            transactions.push(row)
+          })
+          this.datatable.rows.add(transactions);
+          this.datatable.draw();
+
+          // ADD DENOMINATION
+          //   $('tbody').append('<tr><td>' + '' + '</td>' + '<td>' + description + '</td>' + '<td>' + amount + '</td>' + '</tr><tr><td colspan="3"><p>id: ' + value['id'] + '<br>sender/spender: ' + value['sender'] + '<br>from/owner: ' + value['from'] + '<br>receiver: ' + value['receiver'] + '</p></td></tr>')
+          //   // Put denom next to amount header
+          //   $('#denomination').text('(' + value['coins']['denom'] + ')')
+          // })
+          params = {
+            balance: {
+              address: address,
+              key: viewingKey
+            }
+          }
+          let balance_response = await client.queryContractSmart(contractAddress, params);
+          // Display results
+          $('#balance').text(balance_response["balance"]["amount"])
         }
-      }
+        catch(err) {
+          document.showAlertDanger(err)
+        }
+        finally {
+          // Enable form
+          $("#search-button").prop("disabled", false);
+          $("#loading").addClass("d-none")
+          $("#ready").removeClass("d-none")
+        }
+      })();
 
-      document.secretNetworkTransactionsForm.onsubmit = () => {
-        let transactions = [];
-        this.datatable.clear().draw();
-        // Disable form
-        $("#search-button").prop("disabled", true);
-        $("#loading").removeClass("d-none")
-        $("#ready").addClass("d-none")
-        document.hideAllAlerts();
-        (async () => {
-          try {
-            // Set environment
-            let environment = document.featureEnvironment();
-            let client =  document.secretNetworkClient(environment);
-            let address = document.secretNetworkTransactionsForm.address.value;
-            let contractAddress = secretNetworkTransactionsForm.contractAddress.value;
-            let chainId = document.secretNetworkChainId(environment);
-            let viewingKey = document.secretNetworkTransactionsForm.viewingKey.value;
-            let pageSize = document.secretNetworkTransactionsForm.pageSize.value;
-            let page = document.secretNetworkTransactionsForm.page.value;
-            let params = {
-              transfer_history: {
-                address: address,
-                key: viewingKey,
-                page: parseFloat(page - 1),
-                page_size: parseFloat(pageSize)
-              }
-            }
-            let transactions_response = await client.queryContractSmart(contractAddress, params);
-            _.each(transactions_response["transfer_history"]["txs"], function(value){
-              let row = [];
-              // data
-              let id = value['id']
-              row.push(id);
-              description & amount
-              let amount = value['coins']['amount']
-              let description
-              if (address != value['receiver']) {
-                amount *= -1
-                description = value['receiver']
-              } else {
-                description = value['from']
-              }
-              row.push(description)
-              row.push(parseFloat(amount).toLocaleString('en'))
-              transactions.push(row)
-            })
-            this.datatable.rows.add(transactions);
-            this.datatable.draw();
-
-            // ADD DENOMINATION
-            //   $('tbody').append('<tr><td>' + '' + '</td>' + '<td>' + description + '</td>' + '<td>' + amount + '</td>' + '</tr><tr><td colspan="3"><p>id: ' + value['id'] + '<br>sender/spender: ' + value['sender'] + '<br>from/owner: ' + value['from'] + '<br>receiver: ' + value['receiver'] + '</p></td></tr>')
-            //   // Put denom next to amount header
-            //   $('#denomination').text('(' + value['coins']['denom'] + ')')
-            // })
-            params = {
-              balance: {
-                address: address,
-                key: viewingKey
-              }
-            }
-            let balance_response = await client.queryContractSmart(contractAddress, params);
-            // Display results
-            $('#balance').text(balance_response["balance"]["amount"])
-          }
-          catch(err) {
-            document.showAlertDanger(err)
-          }
-          finally {
-            // Enable form
-            $("#search-button").prop("disabled", false);
-            $("#loading").addClass("d-none")
-            $("#ready").removeClass("d-none")
-          }
-        })();
-
-        return false;
-      };
+      return false;
     }
 
     function initListeners() {
