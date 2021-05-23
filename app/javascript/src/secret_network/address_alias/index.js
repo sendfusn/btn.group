57a1a5eb377069b716af9842e9e1ce69d562af0e
Steven Chang <stevenchang5000@protonmail.com>
2021-05-23 12:59:09 +1000

    [BG-76] Enable deleting of address alias (#161)
    Co-authored-by: Steven Chang <stevenchang5000@gmail.com>

--- app/javascript/src/secret_network/address_alias/index.js
+++ app/javascript/src/secret_network/address_alias/index.js
@@ -1,5 +1,9 @@
 $(document).ready(function(){
   if($("#secret-network-address-alias-index").length) {
+    const {
+      SigningCosmWasmClient,
+    } = require('secretjs');
+
     window.onload = async () => {
       document.aliasSearchForm.onsubmit = () => {
         $("#search-button").prop("disabled", true);
@@ -11,6 +15,7 @@ $(document).ready(function(){
           try {
             // Set environment
             let environment = document.featureEnvironment();
+            let chainId = document.secretNetworkChainId(environment)
             let client =  document.secretNetworkClient(environment);
             let contractAddress = document.featureContractAddress(environment);
             let search_type = document.aliasSearchForm.searchType.value;
@@ -19,9 +24,12 @@ $(document).ready(function(){
             let result = await client.queryContractSmart(contractAddress, { search: search_params })
             $("#result-value").removeClass("d-none");
             // $("#result-value").html(document.prettyPrintJSON(result));
-            let url = 'https://secretnodes.com/secret/chains/' + document.secretNetworkChainId(environment) + '/accounts/' + result['attributes']['address']
+            let url = 'https://secretnodes.com/secret/chains/' + chainId + '/accounts/' + result['attributes']['address']
             let resultValueHtml = '<h3 class="mb-0">' + result['attributes']['alias'] + '</h3><a class="mb-3 d-block" target="_blank" rel="noopener" href="' + url + '">' + result['attributes']['address'] + '</a><img class="w-100" src="' + result['attributes']['avatar_url'] + '">'
             $("#result-value").html(resultValueHtml)
+            // Set data on delete button
+            $("#delete-button").data('environment', environment)
+            $("#delete-button").data('alias', result['attributes']['alias'])
           }
           catch(err) {
             let errorDisplayMessage = err;
@@ -42,5 +50,77 @@ $(document).ready(function(){
         return false;
       };
     }
+
+    $("#delete-button").click(function(){
+      (async () => {
+        $("#delete-button").prop("disabled", true);
+        $("#delete-loading").removeClass("d-none")
+        $("#delete-ready").addClass("d-none")
+        try {
+          // Set environment
+          let environment = $("#delete-button").data('environment');
+          let contractAddress = document.featureContractAddress(environment);
+          let chainId = document.secretNetworkChainId(environment)
+          let httpUrl = document.secretNetworkHttpUrl(environment)
+
+          // Keplr extension injects the offline signer that is compatible with cosmJS.
+          // You can get this offline signer from `window.getOfflineSigner(chainId:string)` after load event.
+          // And it also injects the helper function to `window.keplr`.
+          // If `window.getOfflineSigner` or `window.keplr` is null, Keplr extension may be not installed on browser.
+          if (!window.getOfflineSigner || !window.keplr) {
+            alert("Please install keplr extension");
+          } else {
+            if (window.keplr.experimentalSuggestChain) {
+              try {
+                // This method will ask the user whether or not to allow access if they haven't visited this website.
+                // Also, it will request user to unlock the wallet if the wallet is locked.
+                // If you don't request enabling before usage, there is no guarantee that other methods will work.
+                await window.keplr.enable(chainId);
+
+                // @ts-ignore
+                const keplrOfflineSigner = window.getOfflineSigner(chainId);
+                const accounts = await keplrOfflineSigner.getAccounts();
+                this.address = accounts[0].address;
+                this.client = new SigningCosmWasmClient(
+                  httpUrl,
+                  this.address,
+                  keplrOfflineSigner,
+                  window.getEnigmaUtils(chainId),
+                  {
+                    init: {
+                      amount: [{ amount: '300000', denom: 'uscrt' }],
+                      gas: '300000',
+                    },
+                    exec: {
+                      amount: [{ amount: '300000', denom: 'uscrt' }],
+                      gas: '300000',
+                    },
+                  },
+                );
+                this.account = await this.client.getAccount(this.address);
+              } catch (error) {
+                console.error(error)
+              }
+            } else {
+              alert("Please use the recent version of keplr extension");
+            }
+          }
+
+          let alias = $("#delete-button").data('alias');
+          let handleMsg = { destroy: { alias: alias } }
+          let response = await this.client.execute(contractAddress, handleMsg)
+          document.showAlertSuccess('Alias destroyed.')
+        }
+        catch(err) {
+          document.showAlertDanger(err)
+        }
+        finally {
+          $("#result-container").removeClass("d-none");
+          $("#delete-button").prop("disabled", false);
+          $("#delete-loading").addClass("d-none")
+          $("#delete-ready").removeClass("d-none")
+        }
+      })();
+    })
   };
 });
