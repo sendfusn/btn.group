5204ccb12d99bc89202e4229e00eff98c6dc4016
Steven Chang <stevenchang5000@gmail.com>
2021-07-15 19:34:38 +1000

    ApplicationJob: #scheduled?

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -6,4 +6,8 @@ class ApplicationJob < ActiveJob::Base
 
   # Most jobs are safe to ignore if the underlying records are no longer available
   # discard_on ActiveJob::DeserializationError
+
+  def self.scheduled?
+    Sidekiq::ScheduledSet.new.any? { |job| job.item['wrapped'] == name }
+  end
 end
