0b949fb153a98fa44e0f739a366b48c9f4054664
Steven Chang <steven.chang@btn.group>
2021-08-19 12:06:26 +1000

    Setup datahub controller for near (#220)

--- app/controllers/datahub_controller.rb
+++ app/controllers/datahub_controller.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+# Move this to secret_network namespace when you get the time. No hurry.
+
 class DatahubController < ApplicationController
   include ReverseProxy::Controller
 
---
+++ app/controllers/near/datahub_controller.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+
+module Near
+  class DatahubController < ApplicationController
+    include ReverseProxy::Controller
+
+    skip_before_action :verify_authenticity_token
+
+    def index
+      datahub_url = "https://near-mainnet--rpc--archive.datahub.figment.io/apikey/#{Rails.application.credentials.datahub_api_key}"
+      path = request.fullpath.split('datahub').second
+      reverse_proxy datahub_url, path: path, headers: { 'HOST' => nil } do |config|
+        # We got a 404!
+        config.on_missing do |_code, _response|
+          return redirect_to root_url
+        end
+
+        config.on_success do |_code, response|
+          return render json: JSON.parse(response.body)
+        end
+
+        # There's also other callbacks:
+        # - on_set_cookies
+        # - on_connect
+        # - on_response
+        # - on_set_cookies
+        # - on_success
+        # - on_redirect
+        # - on_missing
+        # - on_error
+        # - on_complete
+      end
+    end
+
+    def index_staging
+      datahub_url = "https://near-testnet--rpc.datahub.figment.io/apikey/#{Rails.application.credentials.datahub_api_key}"
+      path = request.fullpath.split('datahub_staging').second
+      reverse_proxy datahub_url, path: path, headers: { 'HOST' => nil } do |config|
+        # We got a 404!
+        config.on_missing do |_code, _response|
+          return redirect_to root_url
+        end
+
+        config.on_success do |_code, response|
+          return render json: JSON.parse(response.body)
+        end
+      end
+    end
+  end
+end
