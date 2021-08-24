6c9d1fbd1020f0555b8de5b34573e0f1a5b91e4b
Steven Chang <steven.chang@btn.group>
2021-08-24 15:56:55 +1000

    Mount Doom: Enable setting viewing key for token (#226)

--- app/javascript/src/secret_network/mount_doom/index.js
+++ app/javascript/src/secret_network/mount_doom/index.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
   if($("#secret-network-mount-doom").length) {
       // Listeners
-      $('input[type=radio][name=functionType]').on('change', function() {
+      $('input[type=radio][name=interactionType]').on('change', function() {
         var radVal = $(this).val();
         if (radVal == 'query') {
           $('#contract-hash-input-group').addClass('d-none')
@@ -27,14 +27,59 @@ $(document).ready(function(){
             let chainId = document.secretNetworkChainId(environment)
             let client =  document.secretNetworkClient(environment);
             let contractAddress = document.featureContractAddress(environment);
+            let httpUrl = document.secretNetworkHttpUrl(environment)
             let tokenAddress = document.mountDoomForm.tokenAddress.value;
-            let contractHash = document.mountDoomForm.contractHash.value;
-            let params = { balance:{ address: contractAddress, contract_hash: contractHash, key: "DoTheRightThing." } };
-            let balance_response = await client.queryContractSmart(tokenAddress, params)
-            if (balance_response["viewing_key_error"]) {
-              throw balance_response["viewing_key_error"]["msg"]
+            if(document.mountDoomForm.interactionType.value == 'query') {
+              let msg = { balance:{ address: contractAddress, key: "DoTheRightThing." } };
+              let balance_response = await client.queryContractSmart(tokenAddress, msg)
+              if (balance_response["viewing_key_error"]) {
+                throw balance_response["viewing_key_error"]["msg"]
+              }
+              document.showAlertSuccess(balance_response["balance"]["amount"]);
+            } else {
+              const {
+                SigningCosmWasmClient,
+              } = require('secretjs');
+              let contractHash = document.mountDoomForm.contractHash.value;
+              let msg = { set_viewing_key_for_snip20: { address: tokenAddress, contract_hash: contractHash } };
+              if (!window.getOfflineSigner || !window.keplr) {
+                alert("Please install keplr extension");
+              } else {
+                if (window.keplr.experimentalSuggestChain) {
+                  try {
+                    // This method will ask the user whether or not to allow access if they haven't visited this website.
+                    // Also, it will request user to unlock the wallet if the wallet is locked.
+                    // If you don't request enabling before usage, there is no guarantee that other methods will work.
+                    await window.keplr.enable(chainId);
+
+                    // @ts-ignore
+                    const keplrOfflineSigner = window.getOfflineSigner(chainId);
+                    const accounts = await keplrOfflineSigner.getAccounts();
+                    this.address = accounts[0].address;
+                    this.client = new SigningCosmWasmClient(
+                      httpUrl,
+                      this.address,
+                      keplrOfflineSigner,
+                      window.getEnigmaUtils(chainId),
+                      {
+                        exec: {
+                          amount: [{ amount: '500000', denom: 'uscrt' }],
+                          gas: '500000',
+                        },
+                      },
+                    );
+                    this.account = await this.client.getAccount(this.address);
+                  } catch (error) {
+                    console.error(error)
+                  }
+                } else {
+                  alert("Please use the recent version of keplr extension");
+                }
+              }
+
+              result = await this.client.execute(contractAddress, msg)
+              document.showAlertSuccess("Viewing key \"DoTheRightThing.\" set.");
             }
-            $('#result-value').text(applyDecimals(balance_response["balance"]["amount"], token_decimals) + ' ' + token_symbol)
           }
           catch(err) {
             document.showAlertDanger(err)
@@ -49,6 +94,10 @@ $(document).ready(function(){
 
         return false;
       };
+    }
+
+    function applyDecimals(amount, decimals) {
+      return amount / parseFloat("1" + '0'.repeat(decimals))
     }
   };
 });
