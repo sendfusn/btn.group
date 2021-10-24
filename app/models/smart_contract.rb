a694b7b1e11180809b43e895682f0931a5fbc508
Steven Chang <steven.chang@btn.group>
2021-10-25 10:06:44 +1100

    Smart contract add enum (#370)
    * Fix database issues
    
    * SmartContract: create alias_attribute for data_hash to contract_hash

--- app/models/smart_contract.rb
+++ app/models/smart_contract.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class SmartContract < ApplicationRecord
-  # === ALIAS ===
-  alias contract_hash data_hash
+  # === ALIAS ATTRIBUTE ===
+  alias_attribute :contract_hash, :data_hash
 
   # === ASSOCIATIONS ===
   belongs_to :blockchain
