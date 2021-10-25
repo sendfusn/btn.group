6159283b673531233850ede9da9fe7d783cbdcb1
Steven Chang <steven.chang@btn.group>
2021-10-25 11:47:00 +1100

    Smart contracts: add unique index for address (#371)

---
+++ db/migrate/20211025002258_add_unique_index_for_address_for_smart_contracts.rb
@@ -0,0 +1,5 @@
+class AddUniqueIndexForAddressForSmartContracts < ActiveRecord::Migration[6.1]
+  def change
+    add_index :smart_contracts, :address, unique: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_15_090806) do
+ActiveRecord::Schema.define(version: 2021_10_25_002258) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,6 +66,7 @@ ActiveRecord::Schema.define(version: 2021_07_15_090806) do
     t.string "data_hash"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["address"], name: "index_smart_contracts_on_address", unique: true
     t.index ["blockchain_id"], name: "index_smart_contracts_on_blockchain_id"
   end
 
