ee026b4951ee50ec0072d46c078874937934aea1
Steven Chang <steven.chang@btn.group>
2021-10-27 22:16:31 +1100

    Transactions select for token (#397)
    * Transactions: put tokens into a select drop down
    
    * Transactions: select for token and remove selectize

--- app/models/cryptocurrency.rb
+++ app/models/cryptocurrency.rb
@@ -4,8 +4,15 @@ class Cryptocurrency < ApplicationRecord
   # === ASSOCIATIONS ===
   belongs_to :smart_contract, optional: true
 
+  # === DELEGATES ===
+  delegate :address, to: :smart_contract
+
   # === VALIDATIONS ===
   validates :decimals, numericality: { greater_than_or_equal_to: 0 }
   validates :price, numericality: { greater_than_or_equal_to: 0, allow_nil: true }
   validates :smart_contract_id, uniqueness: { allow_nil: true }
+
+  def label_formatted
+    "#{name} (#{symbol}) - #{address}"
+  end
 end
