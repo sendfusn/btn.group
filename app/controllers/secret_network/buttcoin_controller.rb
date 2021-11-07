b1f504ff9b1e9e74da47bccec49c25f0dfb003cb
Steven Chang <steven.chang@btn.group>
2021-11-07 21:08:56 +1100

    Buttcoin circulating supply: api endpoint to factor in when no more BUTT to distribute (#458)

--- app/controllers/secret_network/buttcoin_controller.rb
+++ app/controllers/secret_network/buttcoin_controller.rb
@@ -9,8 +9,10 @@ module SecretNetwork
     def circulating_supply
       response = RestClient.get 'https://api.secretapi.io/blocks/latest'
       height = JSON.parse(response.body)['block']['header']['height']
+      circulating_supply = 28_000_006
+      circulating_supply -= (15_060_000 - height.to_i) * 1.4 if height.to_i < 15_060_000
       render json: {
-        circulating_supply: (28_000_006 - ((15_060_000 - height.to_i) * 1.4)).round(1).to_s,
+        circulating_supply: circulating_supply.round(1).to_s,
         height: height
       }
     end
