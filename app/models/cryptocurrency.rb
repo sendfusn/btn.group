a693e0cf9b233fb7642e6d455d2cbd293aa6d8a8
Steven Chang <steven.chang@btn.group>
2021-11-11 15:06:09 +1100

    DEX aggregator: get tokens that have volume and set into select (#498)

--- app/models/cryptocurrency.rb
+++ app/models/cryptocurrency.rb
@@ -4,15 +4,26 @@ class Cryptocurrency < ApplicationRecord
   # === ASSOCIATIONS ===
   belongs_to :blockchain
   belongs_to :smart_contract, optional: true
+  has_many :cryptocurrency_pools, dependent: :restrict_with_exception
 
   # === DELEGATES ===
   delegate :address, to: :smart_contract
+
+  # === SCOPES ===
+  scope :secret_network, lambda { where(blockchain: Blockchain.find_by(identifier: 'secret_network')) }
+  scope :tradeable, lambda {
+    joins(:cryptocurrency_pools)
+      .where(cryptocurrency_pools: { cryptocurrency_role: 'deposit' })
+      .where.not(cryptocurrency_pools: { total: ['0', nil] })
+      .uniq
+  }
 
   # === VALIDATIONS ===
   validates :decimals, numericality: { greater_than_or_equal_to: 0 }
   validates :price, numericality: { greater_than_or_equal_to: 0, allow_nil: true }
   validates :smart_contract_id, uniqueness: { allow_nil: true }
 
+  # === INSTANCE METHODS ===
   def label_formatted
     "#{symbol} / #{name} / #{address}"
   end
--- spec/models/cryptocurrency_spec.rb
+++ spec/models/cryptocurrency_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe Cryptocurrency, type: :model do
   describe 'ASSOCIATIONS' do
     it { should belong_to(:blockchain) }
     it { should belong_to(:smart_contract).optional(true) }
+    it { should have_many(:cryptocurrency_pools).dependent(:restrict_with_exception) }
   end
 
   describe 'VALIDATIONS' do
