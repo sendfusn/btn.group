3a2989d3f8b6a8fd120140cb36c84538d5880bdd
Steven Chang <steven.chang@btn.group>
2021-11-28 22:57:30 +1100

    Redis: update initializer to work with redis to go (#592)
    * Redis: update initializer to work with redis to go
    
    * Redis upgrade

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,5 +1,4 @@
 # frozen_string_literal: true
 
-# from https://devcenter.heroku.com/articles/heroku-redis#connecting-in-ruby
-# https://help.heroku.com/HC0F8CUS/redis-connection-issues
-$redis = Redis.new(url: ENV['REDIS_URL'], ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE })
+uri = URI.parse(ENV['REDISTOGO_URL'])
+REDIS = Redis.new(url: uri)
