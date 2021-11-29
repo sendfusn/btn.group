3785146bc3799ab57e1fc01ce6040272c5059c07
Steven Chang <steven.chang@btn.group>
2021-11-29 17:42:51 +1100

    Redis config to apply to production environment only (#596)

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,15 +1,18 @@
 # frozen_string_literal: true
-#https://github.com/mperham/sidekiq/wiki/Heroku
 
-SIDEKIQ_REDIS_CONFIGURATION = {
-  url: ENV[ENV["REDIS_PROVIDER"]], # if one assumes that REDIS_PROVIDER indirection is reliably present
-  ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }, # we must trust Heroku and AWS here
-}
+# https://github.com/mperham/sidekiq/wiki/Heroku
 
-Sidekiq.configure_server do |config|
-  config.redis = SIDEKIQ_REDIS_CONFIGURATION
-end
+if Rails.env.production?
+  SIDEKIQ_REDIS_CONFIGURATION = {
+    url: ENV[ENV['REDIS_PROVIDER']], # if one assumes that REDIS_PROVIDER indirection is reliably present
+    ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } # we must trust Heroku and AWS here
+  }.freeze
 
-Sidekiq.configure_client do |config|
-  config.redis = SIDEKIQ_REDIS_CONFIGURATION
+  Sidekiq.configure_server do |config|
+    config.redis = SIDEKIQ_REDIS_CONFIGURATION
+  end
+
+  Sidekiq.configure_client do |config|
+    config.redis = SIDEKIQ_REDIS_CONFIGURATION
+  end
 end
