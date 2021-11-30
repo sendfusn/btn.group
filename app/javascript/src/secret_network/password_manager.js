91fa19984b9ca5e7ccdea245894f24d89b8b8f9b
Steven Chang <steven.chang@btn.group>
2021-12-01 00:31:37 +1100

    Password manager: enable revealing an authentication (#602)

--- app/javascript/src/secret_network/password_manager.js
+++ app/javascript/src/secret_network/password_manager.js
@@ -80,14 +80,64 @@ $(document).ready(function(){
         this.setPasswordManagerUpdateForm()
       }.bind(this))
 
-      $('button .fa-eye').parent().click(function(e){
-        e.preventDefault()
-        this.authentications[this.chosenAuthenticationId].username = 'abc'
-        this.authentications[this.chosenAuthenticationId].password = 'def'
-        this.authentications[this.chosenAuthenticationId].notes = 'ghi'
-        this.setPasswordManagerUpdateForm()
-        this.setShowTableData()
-        $('button .fa-eye').parent().addClass('d-none')
+      $('button .fa-eye').closest('button').click(function(e){
+        $("#tab-2-3 button .loading").removeClass('d-none');
+        $("#tab-2-3 button .ready").addClass('d-none');
+        (async () => {
+          try {
+            // Keplr extension injects the offline signer that is compatible with cosmJS.
+            // You can get this offline signer from `window.getOfflineSigner(this.chainId:string)` after load event.
+            // And it also injects the helper function to `window.keplr`.
+            // If `window.getOfflineSigner` or `window.keplr` is null, Keplr extension may be not installed on browser.
+            if (!window.getOfflineSigner || !window.keplr) {
+              alert("Please install keplr extension");
+            } else {
+              if (window.keplr.experimentalSuggestChain) {
+                try {
+                  // This method will ask the user whether or not to allow access if they haven't visited this website.
+                  // Also, it will request user to unlock the wallet if the wallet is locked.
+                  // If you don't request enabling before usage, there is no guarantee that other methods will work.
+                  await window.keplr.enable(this.chainId);
+
+                  // @ts-ignore
+                  const keplrOfflineSigner = window.getOfflineSigner(this.chainId);
+                  const accounts = await keplrOfflineSigner.getAccounts();
+                  this.address = accounts[0].address;
+                  let gasParams = {
+                      exec: {
+                        amount: [{ amount: '50000', denom: 'uscrt' }],
+                        gas: '50000',
+                      },
+                    }
+                  this.client = document.secretNetworkSigningClient(this.environment, this.address, gasParams)
+                } catch (error) {
+                  document.showAlertDanger(error)
+                }
+              } else {
+                alert("Please use the recent version of keplr extension");
+              }
+            }
+            let handleMsg = { show: { id: Number(this.chosenAuthenticationId) } }
+            let response = await this.client.execute(this.contractAddress, handleMsg)
+            let resultText = '';
+            response['data'].forEach(function(x){ resultText += String.fromCharCode(x) })
+            let authentication = JSON.parse(resultText)['show']['authentication']
+            this.authentications[this.chosenAuthenticationId].username = authentication['username']
+            this.authentications[this.chosenAuthenticationId].password = authentication['password']
+            this.authentications[this.chosenAuthenticationId].notes = authentication['notes']
+            this.authentications[this.chosenAuthenticationId].revealed = true
+            this.setPasswordManagerUpdateForm()
+            this.setShowTableData()
+            document.showAlertSuccess('Revealed')
+          }
+          catch(err) {
+            document.showAlertDanger(err)
+          }
+          finally {
+            $("#tab-2-3 button .loading").addClass('d-none');
+            $("#tab-2-3 button .ready").removeClass('d-none');
+          }
+        })();
       }.bind(this))
 
       // === FORMS ===
