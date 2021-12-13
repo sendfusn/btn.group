76dfa87f1c299c89eadc71f531686641a072dc88
Steven Chang <steven.chang@btn.group>
2021-12-13 20:10:14 +1100

    DEX aggregator: get and set trade pairs and cryptocurrencies (#659)

--- app/controllers/pools_controller.rb
+++ app/controllers/pools_controller.rb
@@ -7,6 +7,6 @@ class PoolsController < ApplicationController
     @pools = Pool.where(category: :trade_pair)
                  .includes(:smart_contract)
                  .where(smart_contract: { blockchain_id: Blockchain.find_by(identifier: :secret_network) })
-    render json: @pools, include: { smart_contract: {}, cryptocurrency_pools: {} }
+    render json: @pools, include: { protocol: {}, smart_contract: {}, cryptocurrency_pools: { include: { cryptocurrency: {} } } }
   end
 end
