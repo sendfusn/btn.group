fb453c1a4c4e63dabf7330550e19a73f25ec5a5f
Steven Chang <steven.chang@btn.group>
2021-12-14 13:30:26 +1100

    trade_pairs.js to query blockchain for pair_info (#668)

--- app/javascript/src/secret_network/trade_pairs.js
+++ app/javascript/src/secret_network/trade_pairs.js
@@ -14,8 +14,26 @@ $(document).ready(function(){
           type: 'GET'
         })
         this.tradePairs = result
-        console.log(this.tradePairs)
         $("#results").html(123)
+        console.log(this.tradePairs)
+        for (const el of this.tradePairs) {
+          await this.queryPoolFromBlockchain(el);
+        }
+      }
+
+      this.queryPoolFromBlockchain = async(tradePair) => {
+        let protocolIdentifier = tradePair['protocol']['identifier']
+        let address = tradePair['smart_contract']['address']
+        let msg = { pool: {} }
+        if (protocolIdentifier == 'sienna') {
+          msg = 'pair_info'
+        }
+        try {
+          let result = await this.client.queryContractSmart(address, msg);
+          console.log(result)
+        } catch (error) {
+          console.error(error)
+        }
       }
 
       this.getAndSetTradePairs()
