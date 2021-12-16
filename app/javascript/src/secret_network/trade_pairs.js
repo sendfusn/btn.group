e6d81e37c516e4556d73618de464a5c464389e22
Steven Chang <steven.chang@btn.group>
2021-12-16 12:08:44 +1100

    Show trade pair results in tables (#683)

--- app/javascript/src/secret_network/trade_pairs.js
+++ app/javascript/src/secret_network/trade_pairs.js
@@ -7,6 +7,24 @@ $(document).ready(function(){
       this.chainId = document.secretNetworkChainId(this.environment);
       this.client = document.secretNetworkClient(this.environment);
       this.httpUrl = document.secretNetworkHttpUrl(this.environment)
+      // datatable
+      this.datatable = window.$('#trade-pairs-table').DataTable({
+        columns: [
+            { data: 'poolId', title: "ID" },
+            { data: 'poolAddress', title: "Trade pair" },
+            { data: 'crypto0Address', title: "Crypto 0" },
+            { data: 'crypto1Address', title: "Crypto 1" },
+            { data: 'crypto0Amount', title: "Crypto 0 amount" },
+            { data: 'crypto1Amount', title: "Crypto 1 amount" },
+        ],
+        dom: '<"top"i>frtp',
+        ordering: false,
+        paging: false,
+        rowId: function(a) {
+          return 'position_' + a['position'];
+        },
+      });
+      this.tradePairsFormatted = []
 
       this.getAndSetTradePairs = async() => {
         let result = await $.ajax({
@@ -28,6 +46,7 @@ $(document).ready(function(){
           if (protocolIdentifier == 'sienna') {
             msg = 'pair_info'
           }
+          let tradePairForDataTable = {}
           try {
             let result = await this.client.queryContractSmart(address, msg);
             let asset_0_address;
@@ -38,9 +57,9 @@ $(document).ready(function(){
             if (protocolIdentifier == 'sienna') {
               asset_0_address = result['pair_info'][
               'pair']['token_0']['custom_token']['contract_addr']
-              asset_0_amount == result['pair_info']['amount_0']
+              asset_0_amount = result['pair_info']['amount_0']
               asset_1_address = result['pair_info']['pair']['token_1']['custom_token']['contract_addr']
-              asset_1_amount == result['pair_info']['amount_1']
+              asset_1_amount = result['pair_info']['amount_1']
             } else if (protocolIdentifier == 'secret_swap') {
               if (result['assets'][0]['info']['token']) {
                 asset_0_address = result['assets'][0]['info']['token']['contract_addr']
@@ -90,13 +109,24 @@ $(document).ready(function(){
                       dataType: 'json'
                     })
                   } catch(error) {
-                    console.log(error)
+                    document.showAlertDanger(error)
                   }
                 }
               }
             }
+
+            tradePairForDataTable['poolId'] = tradePair['id']
+            tradePairForDataTable['poolAddress'] = address
+            tradePairForDataTable['crypto0Address'] = asset_0_address
+            tradePairForDataTable['crypto0Amount'] = asset_0_amount
+            tradePairForDataTable['crypto1Address'] = asset_1_address
+            tradePairForDataTable['crypto1Amount'] = asset_1_amount
+            this.tradePairsFormatted.unshift(tradePairForDataTable)
+            this.datatable.clear()
+            this.datatable.rows.add(this.tradePairsFormatted);
+            this.datatable.draw();
           } catch (error) {
-            console.error(error)
+            document.showAlertDanger(error)
           }
         }
       }
