3fda10eefa1d08ff299bda0c1dc06dd87c87e221
Steven Chang <steven.chang@btn.group>
2021-12-17 13:52:15 +1100

    SwapPathsController: Only allow getting arbitrage swap paths if admin user (#704)

--- app/controllers/swap_paths_controller.rb
+++ app/controllers/swap_paths_controller.rb
@@ -1,10 +1,16 @@
 # frozen_string_literal: true
 
 class SwapPathsController < ApplicationController
-  before_action :authenticate_admin_user!, only: :index
+  before_action :authenticate_admin_user!, only: :index, if: :arbitrage?
 
   def index
     @swap_paths = SwapPath.where(from_id: params['from_id'], to_id: params['to_id']).order(:swap_count)
     render json: @swap_paths, methods: :swap_path_as_array, include: { from: {}, to: {} }
   end
+
+  private
+
+    def arbitrage?
+      params['from_id'] == params['to_id']
+    end
 end
