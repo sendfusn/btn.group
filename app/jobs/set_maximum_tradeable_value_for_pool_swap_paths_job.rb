6060442efbe52f6e3ae6b9dcdebe42dc1cba1e72
Steven Chang <steven.chang@btn.group>
2021-12-19 23:07:28 +1100

    SetMaximumTradeableValueForPoolSwapPathsJob to handle no swap path situations (#724)

--- app/jobs/set_maximum_tradeable_value_for_pool_swap_paths_job.rb
+++ app/jobs/set_maximum_tradeable_value_for_pool_swap_paths_job.rb
@@ -3,6 +3,8 @@
 class SetMaximumTradeableValueForPoolSwapPathsJob < ApplicationJob
   def perform(pool_id, pool_swap_path_id)
     psp = PoolSwapPath.where(pool_id: pool_id).where('id > ?', pool_swap_path_id).order(:id).first
+    return if psp.swap_path.nil?
+
     mtv = psp.swap_path.pools.order(:total_locked).first.total_locked
     psp.swap_path.update(maximum_tradeable_value: mtv)
     SetMaximumTradeableValueForPoolSwapPathsJob.perform_later(pool_id, psp.id)
