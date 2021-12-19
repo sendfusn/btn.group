c42f9ba1e00e062af587d34670c1e78795cba0c9
Steven Chang <steven.chang@btn.group>
2021-12-20 10:09:51 +1100

    SetMaximumTradeableValueForPoolSwapPathsJob: refact (#727)

--- app/jobs/set_maximum_tradeable_value_for_pool_swap_paths_job.rb
+++ app/jobs/set_maximum_tradeable_value_for_pool_swap_paths_job.rb
@@ -3,6 +3,7 @@
 class SetMaximumTradeableValueForPoolSwapPathsJob < ApplicationJob
   def perform(pool_id, pool_swap_path_id)
     psp = PoolSwapPath.where(pool_id: pool_id).where('id > ?', pool_swap_path_id).order(:id).first
+    return if psp.nil?
     return if psp.swap_path.nil?
 
     mtv = psp.swap_path.pools.order(:total_locked).first.total_locked
