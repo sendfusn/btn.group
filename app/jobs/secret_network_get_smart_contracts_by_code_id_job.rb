01e0e662e019795b104cf3f44c193a10d73e5507
Steven Chang <steven.chang@btn.group>
2021-12-21 09:39:30 +1100

    Secret network get smart contracts by code id job (#750)

--- app/jobs/secret_network_get_smart_contracts_by_code_id_job.rb
+++ app/jobs/secret_network_get_smart_contracts_by_code_id_job.rb
@@ -7,12 +7,12 @@ class SecretNetworkGetSmartContractsByCodeIdJob < ApplicationJob
     contracts_for_code_id_response = RestClient.get "https://api.secretapi.io/wasm/code/#{code_id}/contracts"
     contracts_for_code_id = JSON.parse(contracts_for_code_id_response)['result']
     contracts_for_code_id&.each do |contract|
-      SmartContract.find_or_create_by!(address: contract['address'],
-                                       blockchain_id: Blockchain.find_by(identifier: 'secret_network').id,
-                                       code_id: code_id,
-                                       creator: contract['creator'],
-                                       data_hash: data_hash,
-                                       label: contract['label'])
+      SmartContract.create(address: contract['address'],
+                           blockchain_id: Blockchain.find_by(identifier: 'secret_network').id,
+                           code_id: code_id,
+                           creator: contract['creator'],
+                           data_hash: data_hash,
+                           label: contract['label'])
     end
   end
 end
