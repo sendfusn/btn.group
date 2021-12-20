cec3530136a0fc878a8ca8368452f1f2e9cd6aac
Steven Chang <steven.chang@btn.group>
2021-12-20 15:31:45 +1100

    Fix set max tradeable value job (#740)

--- app/jobs/set_maximum_tradeable_value_for_pool_swap_paths_job.rb
+++ app/jobs/set_maximum_tradeable_value_for_pool_swap_paths_job.rb
@@ -4,10 +4,13 @@ class SetMaximumTradeableValueForPoolSwapPathsJob < ApplicationJob
   def perform(pool_id, pool_swap_path_id)
     psp = PoolSwapPath.where(pool_id: pool_id).where('id > ?', pool_swap_path_id).order(:id).first
     return if psp.nil?
-    return if psp.swap_path.nil?
 
-    mtv = psp.swap_path.pools.order(:total_locked).first.total_locked
-    psp.swap_path.update(maximum_tradeable_value: mtv)
     SetMaximumTradeableValueForPoolSwapPathsJob.perform_later(pool_id, psp.id)
+    if psp.swap_path.present?
+      mtv = psp.swap_path.pools.order(:total_locked).first.total_locked
+      psp.swap_path.update(maximum_tradeable_value: mtv)
+    else
+      psp.destroy
+    end
   end
 end
