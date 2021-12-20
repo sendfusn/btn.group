7f0b82c4568c95130fbe0a449ec9066513adaefd
Steven Chang <steven.chang@btn.group>
2021-12-20 11:13:36 +1100

    SwapPathsController: order update (#728)

--- app/controllers/swap_paths_controller.rb
+++ app/controllers/swap_paths_controller.rb
@@ -4,7 +4,7 @@ class SwapPathsController < ApplicationController
   before_action :authenticate_admin_user!, only: :index, if: :arbitrage?
 
   def index
-    @swap_paths = SwapPath.where(from_id: params['from_id'], to_id: params['to_id']).where('maximum_tradeable_value >= ?', maximum_tradeable_amount).order(:swap_count).limit(55)
+    @swap_paths = SwapPath.where(from_id: params['from_id'], to_id: params['to_id']).where('maximum_tradeable_value >= ?', maximum_tradeable_amount).order(maximum_tradeable_value: :desc).order(:swap_count).limit(50)
     render json: @swap_paths, methods: :swap_path_as_array, include: { from: {}, to: {} }
   end
 
