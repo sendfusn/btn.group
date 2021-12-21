18ea89c54a561373692949d48941e21af4ddfa96
Steven Chang <steven.chang@btn.group>
2021-12-21 15:48:28 +1100

    Secret network: get smart contracts by code id fix (#760)

--- app/jobs/secret_network_get_smart_contracts_by_code_id_job.rb
+++ app/jobs/secret_network_get_smart_contracts_by_code_id_job.rb
@@ -7,12 +7,14 @@ class SecretNetworkGetSmartContractsByCodeIdJob < ApplicationJob
     contracts_for_code_id_response = RestClient.get "https://api.secretapi.io/wasm/code/#{code_id}/contracts"
     contracts_for_code_id = JSON.parse(contracts_for_code_id_response)['result']
     contracts_for_code_id&.each do |contract|
-      SmartContract.create(address: contract['address'],
-                           blockchain_id: Blockchain.find_by(identifier: 'secret_network').id,
-                           code_id: code_id,
-                           creator: contract['creator'],
-                           data_hash: data_hash,
-                           label: contract['label'])
+      sc = SmartContract.find_or_initialize_by(address: contract['address'])
+      unless sc.persisted?
+        sc.update(blockchain: Blockchain.find_by(identifier: 'secret_network'),
+                  code_id: code_id,
+                  creator: contract['creator'],
+                  data_hash: data_hash,
+                  label: contract['label'])
+      end
     end
   end
 end
