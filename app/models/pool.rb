cd179bcebc5cff97518792bcbd5727b061c1e9bb
Steven Chang <steven.chang@btn.group>
2022-01-02 08:56:48 +1100

    Add SSCRT to arbitrage opps (#874)

--- app/models/pool.rb
+++ app/models/pool.rb
@@ -28,6 +28,7 @@ class Pool < ApplicationRecord
         CreateSwapPathsJob.perform_later
         CreateArbitragePathsJob.perform_later('BUTT')
         CreateArbitragePathsJob.perform_later('SBNB(BSC)')
+        CreateArbitragePathsJob.perform_later('SSCRT')
         CreateArbitragePathsJob.perform_later('SWBTC')
         CreateArbitragePathsJob.perform_later('SXMR')
       end
