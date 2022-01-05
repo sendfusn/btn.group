ac527bcc79b30babefa1ca621c3a0cfbabe8c573
Steven Chang <steven.chang@btn.group>
2022-01-05 19:24:44 +1100

    SwapPaths active admin index formatting (#921)

--- app/admin/swap_paths.rb
+++ app/admin/swap_paths.rb
@@ -3,6 +3,22 @@
 ActiveAdmin.register SwapPath do
   actions :index
 
+  index do
+    id_column
+    column :from
+    column :to
+    column :swap_path_as_string
+    column :maximum_tradeable_value
+    column :protocol
+    column :arbitrage_amount
+    column :arbitrage_profit
+    column :created_at
+    column :updated_at
+    actions
+  end
+
   filter :from
   filter :to
+  filter :arbitrage_amount
+  filter :arbitrage_profit
 end
