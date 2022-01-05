0ec9df53b7d5f1bfa04ff1a3ca45ca2c88d0ba8e
Steven Chang <steven.chang@btn.group>
2022-01-05 15:56:44 +1100

    Update find arbitrage opp job (#917)

--- app/jobs/find_arbitrage_opportunities_job.rb
+++ app/jobs/find_arbitrage_opportunities_job.rb
@@ -11,12 +11,12 @@ class FindArbitrageOpportunitiesJob < ApplicationJob
     current_amount_as_usd = c.amount_as_usd(current_amount)
     largest_maximum_tradeable_value = swap_paths.order(:maximum_tradeable_value).last.maximum_tradeable_value
     swap_paths.each { |sp| sp.update!(arbitrage_amount: 0, arbitrage_profit: 0) }
-    while current_amount_as_usd < largest_maximum_tradeable_value
-      swap_paths.where('maximum_tradeable_value < ?', current_amount_as_usd)
+    while largest_maximum_tradeable_value > current_amount_as_usd
+      swap_paths.where('maximum_tradeable_value > ?', current_amount_as_usd)
                 .each do |sp|
                   net_result_as_usd = sp.net_result_as_usd(sp.simulate_swaps(current_amount))
                   net_profit = net_result_as_usd - current_amount_as_usd
-                  sp.update(arbitrage_amount: current_amount, arbitrage_profit: net_profit) if net_profit > sp.arbitrage_profit
+                  sp.update!(arbitrage_amount: current_amount, arbitrage_profit: net_profit) if net_profit > sp.arbitrage_profit || sp.arbitrage_profit.zero?
                 end
       current_amount += c_ten_dollars_amount
       current_amount_as_usd = c.amount_as_usd(current_amount)
