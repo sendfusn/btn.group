d26df24f0dbcb293f0239113c7f93c5bf5cfa4fa
Steven Chang <steven.chang@btn.group>
2022-01-06 17:22:18 +1100

    Find arb opps refactor (#936)

--- app/jobs/find_arbitrage_opportunities_job.rb
+++ app/jobs/find_arbitrage_opportunities_job.rb
@@ -2,13 +2,18 @@
 
 class FindArbitrageOpportunitiesJob < ApplicationJob
   def perform(cryptocurrency_id)
+    return if SwapPath.where(from_id: cryptocurrency_id, to_id: cryptocurrency_id)
+                      .where('maximum_tradeable_value > ?', 50)
+                      .where('updated_at > ?', Time.current - 1.minute)
+                      .present?
+
     SwapPath.where(from_id: cryptocurrency_id, to_id: cryptocurrency_id)
             .where('maximum_tradeable_value > ?', 50)
-            .where('updated_at < ?', Time.current - 15.minutes)
+            .where('updated_at < ?', Time.current - 5.minutes)
             .each do |sp|
-              SetBestArbitrageOpportunityForSwapPathJob.set(wait_until: Time.current + 5.minutes).perform_later(sp.id) unless Sidekiq::ScheduledSet.new.any? do |job|
-                                                                                                                                job.item['wrapped'] == 'SetBestArbitrageOpportunityForSwapPathJob' && job.args[0]['arguments'].first == sp.id
-                                                                                                                              end
+              SetBestArbitrageOpportunityForSwapPathJob.perform_later(sp.id) unless Sidekiq::ScheduledSet.new.any? do |job|
+                                                                                      job.item['wrapped'] == 'SetBestArbitrageOpportunityForSwapPathJob' && job.args[0]['arguments'].first == sp.id
+                                                                                    end
             end
   end
 end
