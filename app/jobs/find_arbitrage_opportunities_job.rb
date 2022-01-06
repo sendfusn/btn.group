5cb73e6f491526f910298dbdc07b71c0a12948b2
Steven Chang <steven.chang@btn.group>
2022-01-06 17:15:06 +1100

    Refactor find arb job (#935)

--- app/jobs/find_arbitrage_opportunities_job.rb
+++ app/jobs/find_arbitrage_opportunities_job.rb
@@ -4,10 +4,11 @@ class FindArbitrageOpportunitiesJob < ApplicationJob
   def perform(cryptocurrency_id)
     SwapPath.where(from_id: cryptocurrency_id, to_id: cryptocurrency_id)
             .where('maximum_tradeable_value > ?', 50)
+            .where('updated_at < ?', Time.current - 15.minutes)
             .each do |sp|
-              SetBestArbitrageOpportunityForSwapPathJob.set(wait_until: Time.current + 1.minute).perform_later(sp.id) unless Sidekiq::ScheduledSet.new.any? do |job|
-                                                                                                                               job.item['wrapped'] == 'SetBestArbitrageOpportunityForSwapPathJob' && job.args[0]['arguments'].first == sp.id
-                                                                                                                             end
+              SetBestArbitrageOpportunityForSwapPathJob.set(wait_until: Time.current + 5.minutes).perform_later(sp.id) unless Sidekiq::ScheduledSet.new.any? do |job|
+                                                                                                                                job.item['wrapped'] == 'SetBestArbitrageOpportunityForSwapPathJob' && job.args[0]['arguments'].first == sp.id
+                                                                                                                              end
             end
   end
 end
