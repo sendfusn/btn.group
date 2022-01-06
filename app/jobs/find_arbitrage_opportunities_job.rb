e2d3a418e5cbdf4a04646c8e02694966e5a056a9
Steven Chang <steven.chang@btn.group>
2022-01-06 16:10:17 +1100

    Rubocop (#933)

--- app/jobs/find_arbitrage_opportunities_job.rb
+++ app/jobs/find_arbitrage_opportunities_job.rb
@@ -6,8 +6,8 @@ class FindArbitrageOpportunitiesJob < ApplicationJob
             .where('maximum_tradeable_value > ?', 50)
             .each do |sp|
               SetBestArbitrageOpportunityForSwapPathJob.set(wait_until: Time.current + 1.minute).perform_later(sp.id) unless Sidekiq::ScheduledSet.new.any? do |job|
-                                                                                                                                job.item['wrapped'] == 'SetBestArbitrageOpportunityForSwapPathJob' && job.args[0]['arguments'].first == sp.id
-                                                                                                                              end
+                                                                                                                               job.item['wrapped'] == 'SetBestArbitrageOpportunityForSwapPathJob' && job.args[0]['arguments'].first == sp.id
+                                                                                                                             end
             end
   end
 end
