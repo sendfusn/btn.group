6336e8b414300155d194b1293073da5b580b91c7
Steven Chang <steven.chang@btn.group>
2022-01-24 13:01:22 +1100

    Rubocop and haml-lint fix (#1018)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -149,7 +149,7 @@ class ApplicationController < ActionController::Base
 
   protected
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_in, keys: [:otp_attempt])
-  end
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_in, keys: [:otp_attempt])
+    end
 end
