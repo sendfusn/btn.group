f51d6be6c71dcab53580f101b5318b397191a304
Steven Chang <steven.chang@btn.group>
2022-01-25 11:42:43 +1100

    buttonAddress helper function (#1031)

--- app/javascript/src/secret_network/helpers.js
+++ app/javascript/src/secret_network/helpers.js
@@ -1,38 +1,8 @@
-document.secretNetworkClient = function(environment) {
-  const {
-    CosmWasmClient,
-  } = require('secretjs');
-  return new CosmWasmClient(document.secretNetworkHttpUrl(environment))
-}
-
-document.secretNetworkSigningClient = function(environment, walletAddress, gasParams) {
-  const {
-    SigningCosmWasmClient,
-  } = require('secretjs');
-  let chainId = document.secretNetworkChainId(environment)
-  let httpUrl = document.secretNetworkHttpUrl(document.secretNetworkHttpUrl(environment))
-  let keplrOfflineSigner = window.getOfflineSigner(chainId);
-  return new SigningCosmWasmClient(httpUrl, walletAddress, keplrOfflineSigner, window.getEnigmaUtils(chainId), gasParams)
-}
-
-document.secretNetworkChainId = function(environment) {
-  let chainId = 'secret-4'
-  if (environment == 'staging') {
-    chainId = 'pulsar-2'
-  };
-  return chainId
-}
-
-document.secretNetworkHttpUrl = function(environment) {
-  let http_url = '/datahub';
-  if (environment == 'staging') {
-    http_url = http_url + '_staging'
-  };
-  return http_url
+document.buttonAddress = function() {
+  return 'secret1yxcexylwyxlq58umhgsjgstgcg2a0ytfy4d9lt'
 }
 
 document.getAndSetUserVipLevel = async(address, client) => {
-  let buttcoinContractAddress = 'secret1yxcexylwyxlq58umhgsjgstgcg2a0ytfy4d9lt'
   let chainId = document.secretNetworkChainId('production')
   let userVipLevel = 0
   // Set users vip level
@@ -40,10 +10,10 @@ document.getAndSetUserVipLevel = async(address, client) => {
     let params = {
       balance: {
         address: address,
-        key: await window.keplr.getSecret20ViewingKey(chainId, buttcoinContractAddress)
+        key: await window.keplr.getSecret20ViewingKey(chainId, document.buttonAddress())
       }
     }
-    let balance_response = await client.queryContractSmart(buttcoinContractAddress, params);
+    let balance_response = await client.queryContractSmart(document.buttonAddress(), params);
     let balance = balance_response["balance"]["amount"]
     balance = Number(balance)
     if (balance >= 100_000_000_000) {
@@ -63,4 +33,37 @@ document.getAndSetUserVipLevel = async(address, client) => {
     $("#vip-level").text(userVipLevel)
     return userVipLevel
   }
+}
+
+document.secretNetworkChainId = function(environment) {
+  let chainId = 'secret-4'
+  if (environment == 'staging') {
+    chainId = 'pulsar-2'
+  };
+  return chainId
+}
+
+document.secretNetworkClient = function(environment) {
+  const {
+    CosmWasmClient,
+  } = require('secretjs');
+  return new CosmWasmClient(document.secretNetworkHttpUrl(environment))
+}
+
+document.secretNetworkHttpUrl = function(environment) {
+  let http_url = '/datahub';
+  if (environment == 'staging') {
+    http_url = http_url + '_staging'
+  };
+  return http_url
+}
+
+document.secretNetworkSigningClient = function(environment, walletAddress, gasParams) {
+  const {
+    SigningCosmWasmClient,
+  } = require('secretjs');
+  let chainId = document.secretNetworkChainId(environment)
+  let httpUrl = document.secretNetworkHttpUrl(document.secretNetworkHttpUrl(environment))
+  let keplrOfflineSigner = window.getOfflineSigner(chainId);
+  return new SigningCosmWasmClient(httpUrl, walletAddress, keplrOfflineSigner, window.getEnigmaUtils(chainId), gasParams)
 }
