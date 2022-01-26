2fe00caa1db7ed2066a76db52517b0d6fb002d39
Steven Chang <steven.chang@btn.group>
2022-01-26 11:37:38 +1100

    Pools: For non enabled pools, only retrieve pools with liquidity in the first 10 minutes of an hour (#1050)

--- app/controllers/pools_controller.rb
+++ app/controllers/pools_controller.rb
@@ -5,7 +5,12 @@
 class PoolsController < ApplicationController
   def index
     @pools = Pool.where(category: :trade_pair)
-    @pools = @pools.where(enabled: true) if params['enabled'] == 'true'
+    if params['enabled'] == 'true'
+      @pools = @pools.where(enabled: true)
+    elsif Time.current.min < 5
+      pool_ids = CryptocurrencyPool.where('amount != ?', '0').pluck(:pool_id).uniq
+      @pools = @pools.where(id: pool_ids)
+    end
     @pools = @pools.includes(:smart_contract)
                    .where(smart_contract: { blockchain_id: Blockchain.find_by(identifier: :secret_network) })
     render json: @pools, include: { protocol: {}, smart_contract: {}, cryptocurrency_pools: { include: { cryptocurrency: { include: { smart_contract: {}, attachments: {} } } } } }
