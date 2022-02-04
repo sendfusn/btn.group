8fe7879620db523376999c765088a0bfaec0db36
Steven Chang <stevenchang5000@gmail.com>
2022-02-04 14:12:49 +1100

    CreateSwapPathJob: Update to not schedule job if already scheduled (#1174)

--- app/jobs/create_swap_paths_job.rb
+++ app/jobs/create_swap_paths_job.rb
@@ -9,6 +9,10 @@ class CreateSwapPathsJob < ApplicationJob
                              .pluck(:cryptocurrency_id)
                              .uniq
     cryptocurrency_ids.each do |from_id|
+      next if Sidekiq::ScheduledSet.new.any? do |job|
+        job.item['wrapped'] == 'CreateSwapPathsForCryptoJob' && job.args[0]['arguments'].first == from_id
+      end
+
       run_time = Time.zone.now + rand(60).minutes
       CreateSwapPathsForCryptoJob.set(wait_until: run_time).perform_later(from_id)
     end
