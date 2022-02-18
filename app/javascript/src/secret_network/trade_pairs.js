245d0a5be5c113108d5ef7addc6e0b28ca25485b
Steven Chang <stevenchang5000@gmail.com>
2022-02-18 18:23:35 +1100

    Pools controller refactor (#1268)
    * PoolsController: refactor to handle categories
    
    * Trade pairs: update shares amount and update amounts even when coin gecko id is not present

--- app/javascript/src/secret_network/trade_pairs.js
+++ app/javascript/src/secret_network/trade_pairs.js
@@ -7,11 +7,12 @@ $(document).ready(function(){
       // datatable
       this.datatable = window.$('#trade-pairs-table').DataTable({
         columns: [
-            { data: 'poolId', title: "ID" },
-            { data: 'crypto0Symbol', title: "Crypto 0" },
-            { data: 'crypto1Symbol', title: "Crypto 1" },
-            { data: 'crypto0Amount', title: "Crypto 0 amount" },
-            { data: 'crypto1Amount', title: "Crypto 1 amount" },
+            { data: 'poolId', title: 'ID' },
+            { data: 'crypto0Symbol', title: 'Crypto 0' },
+            { data: 'crypto1Symbol', title: 'Crypto 1' },
+            { data: 'crypto0Amount', title: 'Crypto 0 amount' },
+            { data: 'crypto1Amount', title: 'Crypto 1 amount' },
+            { data: 'shares', title: 'Shares' },
         ],
         dom: '<"top"i>frtp',
         ordering: false,
@@ -45,20 +46,16 @@ $(document).ready(function(){
           }
           let tradePairForDataTable = {}
           try {
-            let result = await this.client.queryContractSmart(address, msg, undefined, tradePair['smart_contract']['data_hash']);
             let asset_0_address;
             let asset_1_address;
             let asset_0_symbol;
             let asset_1_symbol;
             let asset_0_amount;
             let asset_1_amount;
-            let coinGeckoIdsPresent = true;
+            let sharesAmount;
             tradePair['cryptocurrency_pools'].forEach((cryptocurrencyPool) => {
               if (cryptocurrencyPool['cryptocurrency_role'] == 'deposit') {
                 let cryptocurrency = cryptocurrencyPool['cryptocurrency']
-                if (!cryptocurrency['coin_gecko_id']) {
-                  coinGeckoIdsPresent = false
-                }
                 if (asset_0_symbol) {
                   asset_1_symbol = cryptocurrency['symbol']
                 } else {
@@ -66,15 +63,14 @@ $(document).ready(function(){
                 }
               }
             })
-            if (!coinGeckoIdsPresent) {
-              return
-            }
+            let result = await this.client.queryContractSmart(address, msg, undefined, tradePair['smart_contract']['data_hash']);
             if (protocolIdentifier == 'sienna') {
               asset_0_address = result['pair_info'][
               'pair']['token_0']['custom_token']['contract_addr']
               asset_0_amount = result['pair_info']['amount_0']
               asset_1_address = result['pair_info']['pair']['token_1']['custom_token']['contract_addr']
               asset_1_amount = result['pair_info']['amount_1']
+              sharesAmount = result['pair_info']['total_liquidity']
             } else if (protocolIdentifier == 'secret_swap') {
               if (result['assets'][0]['info']['token']) {
                 asset_0_address = result['assets'][0]['info']['token']['contract_addr']
@@ -88,6 +84,7 @@ $(document).ready(function(){
                 asset_1_address = ''
               }
               asset_1_amount = result['assets'][1]['amount']
+              sharesAmount = result['total_share']
             }
             for (let cp of tradePair['cryptocurrency_pools']) {
               if (cp['cryptocurrency_role'] == 'deposit') {
@@ -118,6 +115,19 @@ $(document).ready(function(){
                     document.showAlertDanger(error)
                   }
                 }
+              } else if (cp['cryptocurrency_role'] == 'shares') {
+                if (sharesAmount != cp['amount']) {
+                  try {
+                    let updateResult = await $.ajax({
+                      url: '/cryptocurrency_pools/' + cp['id'],
+                      type: 'put',
+                      data: { cryptocurrency_pool: { amount: sharesAmount } },
+                      dataType: 'json'
+                    })
+                  } catch(error) {
+                    document.showAlertDanger(error)
+                  }
+                }
               }
             }
 
@@ -126,6 +136,7 @@ $(document).ready(function(){
             tradePairForDataTable['crypto0Amount'] = asset_0_amount
             tradePairForDataTable['crypto1Symbol'] = asset_1_symbol
             tradePairForDataTable['crypto1Amount'] = asset_1_amount
+            tradePairForDataTable['shares'] = sharesAmount
             this.tradePairsFormatted.unshift(tradePairForDataTable)
             this.datatable.clear()
             this.datatable.rows.add(this.tradePairsFormatted);
