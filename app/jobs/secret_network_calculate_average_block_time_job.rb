ef6ebc64b40c73fa50942e2b2059cf5f802d814f
Steven Chang <stevenchang5000@gmail.com>
2022-02-23 23:32:10 +1100

    Secret Network: show average block time under navbar (#1318)

--- app/jobs/secret_network_calculate_average_block_time_job.rb
+++ app/jobs/secret_network_calculate_average_block_time_job.rb
@@ -4,7 +4,7 @@ require 'rest-client'
 
 class SecretNetworkCalculateAverageBlockTimeJob < ApplicationJob
   after_perform do |_job|
-    run_time = Time.zone.now + 5.minutes
+    run_time = Time.zone.now + 1.minute
     SecretNetworkCalculateAverageBlockTimeJob.set(wait_until: run_time).perform_later unless SecretNetworkCalculateAverageBlockTimeJob.scheduled?
   end
 
@@ -13,20 +13,10 @@ class SecretNetworkCalculateAverageBlockTimeJob < ApplicationJob
     height = JSON.parse(response.body)['block']['header']['height']
     time = JSON.parse(response.body)['block']['header']['time']
 
-    response_two = RestClient.get "https://api.secretapi.io/blocks/#{height.to_i - 1}"
+    response_two = RestClient.get "https://api.secretapi.io/blocks/#{height.to_i - 5}"
     time_two = JSON.parse(response_two.body)['block']['header']['time']
 
-    response_three = RestClient.get "https://api.secretapi.io/blocks/#{height.to_i - 2}"
-    time_three = JSON.parse(response_three.body)['block']['header']['time']
-
-    response_four = RestClient.get "https://api.secretapi.io/blocks/#{height.to_i - 3}"
-    time_four = JSON.parse(response_four.body)['block']['header']['time']
-
-    cumulative_block_time = 0
-    block_times = [(DateTime.parse(time) - DateTime.parse(time_two)) * 24 * 60 * 60, (DateTime.parse(time_two) - DateTime.parse(time_three)) * 24 * 60 * 60, (DateTime.parse(time_three) - DateTime.parse(time_four)) * 24 * 60 * 60].each do |block_time|
-      cumulative_block_time += block_time
-    end
-
-    Blockchain.secret_network.first.update(latest_block_time: cumulative_block_time / block_times.length)
+    average_block_time_of_last_five_blocks = (DateTime.parse(time) - DateTime.parse(time_two)) * 24 * 60 * 60 / 5
+    Blockchain.secret_network.first.update(latest_block_time: average_block_time_of_last_five_blocks)
   end
 end
